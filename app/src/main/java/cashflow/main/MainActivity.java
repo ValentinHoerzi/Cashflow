065fb6583a1e6d375d4f2526814c8368ac21fe7d
Valentin Hörzi <valentin.hoerzi@gmail.com>
2019-03-04 16:03:03 +0100

    added date and only numeric inputs

--- app/src/main/java/cashflow/main/MainActivity.java
+++ app/src/main/java/cashflow/main/MainActivity.java
@@ -1,33 +1,41 @@
 package cashflow.main;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textViewDate;
+    EditText textViewDate;
     Spinner spinnerAusgabenEinnahmen;
-    TextView textViewPrice;
+    EditText textViewPrice;
     Spinner spinnerKatagorie;
     TextView textViewKategorie;
     Button buttonOK;
     TextView textView_displaySum;
-    ListView listView;
     Model model;
-
     ArrayAdapter<String> catAdapter;
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.YYYY");
 
 
     @Override
@@ -40,11 +48,35 @@ public class MainActivity extends AppCompatActivity {
 
     private void initUI() {
         initTextViewS();
+        initTextViewDate();
         initAdapter1();
         refreshAdapterCategories(catAdapter,true);
     }
-    private void initTextViewS() {
+
+    private void initTextViewDate() {
         textViewDate = findViewById(R.id.textViewDate);
+        final Calendar buyDate = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                buyDate.set(Calendar.YEAR,year);
+                buyDate.set(Calendar.MONTH,month);
+                buyDate.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+                textViewDate.setText(simpleDateFormat.format(buyDate.getTime()));
+            }
+        };
+
+        textViewDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this,date,buyDate.get(Calendar.YEAR),buyDate.get(Calendar.MONTH),buyDate.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void initTextViewS() {
         textView_displaySum = findViewById(R.id.textView_displaySum);
         textViewPrice = findViewById(R.id.textViewPrice);
         textViewKategorie = findViewById(R.id.textViewKategorie);
@@ -57,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         spinnerAusgabenEinnahmen = findViewById(R.id.spinnerAusgabenEinnahmen);
         spinnerAusgabenEinnahmen.setAdapter(adapterAusgabenEinnahmen);
     }
+
     private void refreshAdapterCategories(ArrayAdapter<String> catAdapter, boolean init) {
         List<String> categories = new ArrayList<>();
         spinnerKatagorie = findViewById(R.id.spinnerKatagorie);
